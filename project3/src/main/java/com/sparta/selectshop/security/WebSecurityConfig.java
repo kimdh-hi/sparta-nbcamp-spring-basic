79afb6aafc69df21b5bcadc7591093ed6a048faa
kimdaehuyn <zbeld123@gmail.com>
2021-10-02 11:58:08 +0900

    회원가입

---
+++ project3/src/main/java/com/sparta/selectshop/security/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.sparta.selectshop.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/images/**", "/css/**", "/h2-console").permitAll()
+                .antMatchers("/user/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .failureUrl("/user/login/error")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
